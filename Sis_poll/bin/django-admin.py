f152318155cbb9aeefa6c471c6ae688fc2591571
rivalmsr <rivalmusripal@gmail.com>
2019-11-14 17:02:39 +0700

    first commit

---
+++ Sis_poll/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/rivalmsr/Documents/rdeveloper/python3/python_projects/poll_project/Sis_poll/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
